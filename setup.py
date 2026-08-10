2ddc120706737e9f0e2e404d18365f11f9f95446
xentrick <itsnick@gmail.com>
2026-08-10 15:53:36 -0400

    rscapi 2.0.0

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from setuptools import setup, find_packages  # noqa: H301
 # prerequisite: setuptools
 # http://pypi.python.org/pypi/setuptools
 NAME = "rscapi"
-VERSION = "1.1.1"
+VERSION = "2.0.0"
 PYTHON_REQUIRES = ">= 3.10"
 REQUIRES = [
     "python-dateutil >= 2.8.2",
